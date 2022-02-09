87abc178eca856c98fd64b6b1ec623b8f72dd414
Ignacio Tirado <92279938+VlV-515@users.noreply.github.com>
2022-02-09 16:57:45 -0600

    Implementa los observables logged & role en la muestra de contenido

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { LoginService } from './../../../pages/login/services/login.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent {
+  constructor(public readonly loginSvc:LoginService) {}
 }
